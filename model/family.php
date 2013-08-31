ab9b63c166b47d3fff8f9075f91832d42aa3b8f1
Sam Wilson <sam@samwilson.id.a>
2013-08-31 12:22:43 +0800

    Return empty array by default in child list.

--- model/family.php
+++ model/family.php
@@ -2,44 +2,50 @@
 
 class WeRelateCore_family extends WeRelateCore_base {
 
-    protected $tag = 'family';
+	/** @var string */
+	protected $tag = 'family';
 
-//    public function getFather() {
-//        return $this->getSpouse();
-//        $this->load();
-//        var_dump($this->xml);
-//        $title = Title::makeTitle(NS_WERELATECORE_PERSON, $this->xml->husband['title']);
-//        $father = new WeRelateCore_person($title);
-//        return $father;
-//    }
+	/** @var array */
+	protected $children;
 
-    /**
-     * Get the 'husband' or 'wife'.
-     * 
-     * @param WeRelateCore_person $spouse
-     * @return boolean|\WeRelateCore_person
-     * @throws Exception
-     */
-    public function getSpouse($spouse) {
-        if (!$this->load()) return false;
-        if (!in_array($spouse, array('husband', 'wife'))) {
-            throw new Exception("Spouse must be either 'husband' or 'wife'. '$spouse' given.");
-        }
-        if (!isset($this->xml->{$spouse})) {
+	/**
+	* Get the 'husband' or 'wife'.
+	* 
+	* @param WeRelateCore_person $spouse
+	* @return boolean WeRelateCore_person
+	* @throws Exception
+	*/
+	public function getSpouse($spouse) {
+		if (!$this->load()) return false;
+		if (!in_array($spouse, array('husband', 'wife'))) {
+			throw new Exception("Spouse must be either 'husband' or 'wife'. '$spouse' given.");
+		}
+		if (!isset($this->xml->{$spouse})) {
 			return false;
-        }
-        $title = Title::makeTitle(NS_WERELATECORE_PERSON, $this->xml->{$spouse}['title']);
-        $spouse = new WeRelateCore_person($title);
-        return $spouse;
-    }
-    
-    public function getChildren() {
-        if (!$this->load()) return false;
-        $children = array();
-        foreach ($this->xml->child as $child) {
-            $title = Title::makeTitle(NS_WERELATECORE_PERSON, $child['title']);
-            $children[] = new WeRelateCore_person($title);
-        }
-        return $children;
-    }
+		}
+		$title = Title::makeTitle(NS_WERELATECORE_PERSON, $this->xml->{$spouse}['title']);
+		$spouse = new WeRelateCore_person($title);
+		return $spouse;
+	}
+
+	/**
+	 * Get the children of this family.
+	 *
+	 * @return array An array of WeRelateCore_person objects
+	 */
+	public function getChildren() {
+		if (is_array($this->children)) {
+			return $this->children;
+		}
+		$this->children = array();
+		if (!$this->load()) {
+			return $this->children;
+		}
+		foreach ($this->xml->child as $child) {
+			$title = Title::makeTitle(NS_WERELATECORE_PERSON, $child['title']);
+			$this->children[] = new WeRelateCore_person($title);
+		}
+		return $this->children;
+	}
+
 }
